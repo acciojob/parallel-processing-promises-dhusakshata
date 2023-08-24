8d5e717468a9079c8327559159dac6e074604ffa
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-08-24 22:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+  const imagePromises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+      img.src = image.url;
+    });
+  });
+
+  Promise.all(imagePromises)
+    .then(images => {
+      output.innerHTML = "";
+      images.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
